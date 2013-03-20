ff5b0704dc3b7868aa4bb42e02ffd273cda8f808
Matthias Springer <me@matthiasspringer.de>
2013-03-20 17:06:22 +0100

    Refactored inline object views and added editor with code highlighting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def object_dropdown_begin(parameters)
+    _render_template(:partial => "shared/dropdown_object_begin", :locals => parameters)
+  end
+
+  def object_dropdown_end(parameters)
+    _render_template(:partial => "shared/dropdown_object_end", :locals => parameters)
+  end
+
+  helper_method :object_dropdown_begin
+  helper_method :object_dropdown_end
 end
