3c5b0b95bbca097e8bc0c616ed26a33b867e8308
Matthias Springer <me@matthiasspringer.de>
2013-03-21 22:26:00 +0100

    Added pagination for class explorer and refactored class explorer.

--- app/controllers/class_explorer_controller.rb
+++ app/controllers/class_explorer_controller.rb
@@ -2,22 +2,34 @@ require "set"
 
 class ClassExplorerController < ApplicationController
   def index
-    if params[:id]
-      @clazz = ObjectSpace._id2ref(Integer(params[:id]))
-      @max_items = Integer(params[:maxItems])
+    @id = params[:id] ? Integer(params[:id]) : Class.object_id
+    @clazz = ObjectSpace._id2ref(@id)
+    @items_per_page = 10
+    @page = params[:page] ? Integer(params[:page]) : 1
+     
+    Maglev.abort_transaction
+    @objs = ObjectSpace::SystemRepository.__list_instances([@clazz], 0, nil, 2, 95, false)[1]
+    @max_pages = (Float(@objs.size) / @items_per_page).ceil
+    @objs = @objs[(@page - 1) * @items_per_page, @items_per_page]
+    
+    @ivs = Set.new
+    @show_ivs = params[:showIvs]
 
-      Maglev.abort_transaction
-      @objs = ObjectSpace::SystemRepository.__list_instances([@clazz], @max_items, nil, 2, 95, false)[1][0, @max_items]
-      @ivs = Set.new
-      @show_ivs = params[:showIvs]
-       
-      if (@show_ivs == "all") 
-        for obj in @objs
-          obj.instance_variables.each do |iv_name|
-            @ivs.add(iv_name)
-          end
+    if (@show_ivs == "all") 
+      for obj in @objs
+        obj.instance_variables.each do |iv_name|
+          @ivs.add(iv_name)
         end
       end
     end
   end
+
+  private
+
+  def page_url(page)
+    "/ClassExplorer/index/#{@id}?showIvs=#{@show_ivs}&page=#{page}"
+  end
+
+  helper_method :page_url
+
 end
