ec1b6225ad6e41999bc0fdba1ccfa999f20313c9
Matthias Springer <me@matthiasspringer.de>
2013-03-21 11:48:43 +0100

    Get basic object information as JSON.

--- app/controllers/object_explorer_controller.rb
+++ app/controllers/object_explorer_controller.rb
@@ -3,4 +3,16 @@ class ObjectExplorerController < ApplicationController
     obj = ObjectSpace._id2ref(Integer(params[:id]))
     render(:partial => "shared/object", :locals => {:object => obj})
   end
+
+  def data
+    obj = ObjectSpace._id2ref(Integer(params[:id]))
+    
+    class_id = obj.class.object_id
+    ivs = {}
+    obj.instance_variables.each do |iv_name|
+      ivs[iv_name] = obj.instance_variable_get(iv_name).object_id
+    end
+
+    render :json => {"id" => Integer(params[:id]), "class" => class_id, "ivs" => ivs}
+  end
 end
