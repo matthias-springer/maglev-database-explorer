4d5894326a68e28de841b14e358c1d728c8a7f2a
Matthias Springer <me@matthiasspringer.de>
2013-03-21 15:22:52 +0100

    Implemented basic object view for one object without ivs.

--- app/controllers/object_explorer_controller.rb
+++ app/controllers/object_explorer_controller.rb
@@ -4,15 +4,25 @@ class ObjectExplorerController < ApplicationController
     render(:partial => "shared/object", :locals => {:object => obj})
   end
 
-  def data
-    obj = ObjectSpace._id2ref(Integer(params[:id]))
+  def object
+    @obj = ObjectSpace._id2ref(Integer(params[:id]))
     
-    class_id = obj.class.object_id
-    ivs = {}
-    obj.instance_variables.each do |iv_name|
-      ivs[iv_name] = obj.instance_variable_get(iv_name).object_id
+    @class_id = @obj.class.object_id
+    @ivs = {}
+    @obj.instance_variables.each do |iv_name|
+      @ivs[iv_name] = @obj.instance_variable_get(iv_name).object_id
     end
 
-    render :json => {"id" => Integer(params[:id]), "class" => class_id, "ivs" => ivs}
+    respond_to do |format|
+      format.html do
+        render(:partial => "object")
+      end
+
+      format.json
+    end
+  end
+
+  def graph
+    @id = Integer(params[:id])
   end
 end
