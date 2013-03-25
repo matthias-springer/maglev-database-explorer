4e3489957050de59d7588964adbc48766e234702
Matthias Springer <me@matthiasspringer.de>
2013-03-25 12:15:06 +0100

    Object view: reference arrows for Hash.

--- app/controllers/object_explorer_controller.rb
+++ app/controllers/object_explorer_controller.rb
@@ -1,7 +1,7 @@
 class ObjectExplorerController < ApplicationController
   def table
     obj = ObjectSpace._id2ref(Integer(params[:id]))
-    render(:partial => "shared/object", :locals => {:object => obj})
+    render(:partial => "shared/object", :locals => {:object => obj, :short => params[:short] == "1" ? true : false})
   end
 
   def object
@@ -19,4 +19,37 @@ class ObjectExplorerController < ApplicationController
   def graph
     @id = Integer(params[:id])
   end
+
+  private
+
+  def references(obj)
+    refs = []
+
+    obj.instance_variables.each do |iv_name|
+      refs << ["iv", iv_name, obj.instance_variable_get(iv_name).object_id]
+    end
+    
+    method_name = "references_#{obj.class.to_s.downcase}"
+    begin
+      refs += self.send(method_name, obj)
+    rescue NoMethodError
+    
+    end
+    
+    return refs
+  end
+
+  helper_method :references
+
+  def references_hash(obj)
+    refs = []
+
+    obj.each do |key, value|
+      refs << ["hash_key", value.object_id, key.object_id]
+      refs << ["hash_value", key.object_id, value.object_id]
+    end
+
+    return refs
+  end
+
 end
